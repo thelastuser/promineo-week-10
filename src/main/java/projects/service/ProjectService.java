426e83993c773d5bdc59220bcc17dd8a6af5ca96
thelastuser <thelastuser@hotmail.com>
2024-10-11 12:45:39 -0400

    initial commit

---
+++ src/main/java/projects/service/ProjectService.java
@@ -0,0 +1,31 @@
+package projects.service;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+import projects.dao.ProjectDao;
+import projects.entity.Project;
+
+public class ProjectService {
+	
+	private ProjectDao projectDao = new ProjectDao();
+
+	public Project addProject(Project project) {
+		return projectDao.insertProject(project);
+	}
+
+	public List<Project> fetchAllProjects() {
+		
+		ProjectDao db = new ProjectDao();
+		
+		return db.fetchAllProjects();
+	}
+
+	public Project fetchProjectById(Integer projectId) {
+		
+		return projectDao.fetchProjectById(projectId).orElseThrow( () -> new NoSuchElementException("Project with project ID =" + projectId + " does not exist."));
+
+	}
+
+}
